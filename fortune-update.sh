215054850ffc1decabd3388742c291360aa5c943
cafebedouin <cafebedouin@gmail.com>
2022-09-27 15:04:30 -0500

    Automating system updates.

---
+++ fortune-update.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo cp ~/bin/fortune/zuihitsu /usr/share/games/fortunes/;
+sudo strfile /usr/share/games/fortunes/zuihitsu;
+sudo chmod og+r /usr/share/games/fortunes/zuihitsu;
+sudo chmod og+r /usr/share/games/fortunes/zuihitsu.dat
