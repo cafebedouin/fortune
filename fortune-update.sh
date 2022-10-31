1fc94b5ee7b252f2d8b2ebb507484d5fda251dba
cafebedouin <cafebedouin@gmail.com>
2022-10-31 11:35:06 -0500

    Added line to run fortune to make sure it works.

--- fortune-update.sh
+++ fortune-update.sh
@@ -4,3 +4,4 @@ sudo cp ~/bin/fortune/zuihitsu /usr/share/games/fortunes/;
 sudo strfile /usr/share/games/fortunes/zuihitsu;
 sudo chmod og+r /usr/share/games/fortunes/zuihitsu;
 sudo chmod og+r /usr/share/games/fortunes/zuihitsu.dat
+fortune zuihitsu
